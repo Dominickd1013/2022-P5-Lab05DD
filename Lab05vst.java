ba186d7ff76ebaa3bb66010db67b0c9c539722de
Dominick <domenico.damato@vtsd.com>
2022-10-27 12:29:05 -0400

    New

--- Lab05vst.java
+++ Lab05vst.java
@@ -13,25 +13,26 @@ public class Lab05vst extends Applet
         int width = 980;
         int height = 630;
         int x1 = 50;
-        int y1 = 640;
+        int y1 = 630;
         int x2 = 990;
         int y2 = 600;
         g.drawRect(10,10,width,height);
 
+        {
         int x = 1;
 
-        {
+            while (x = 1, <= 49, x++) {
 
-            if(x >= 1) {
+                int lineheight = 600 - 10;
+                // int h2 = lineheight - 10;
 
-                g.drawLine(x1, y1, x2, y2);
+                g.drawLine(x1, y1, x2, lineheight);
 
+                // x = 1 + 1;
 
 
             }
-
-
-
         }
+
     }
 }
