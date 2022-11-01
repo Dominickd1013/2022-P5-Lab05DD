cdd122e5a4c961600d7beaba1e9e9f992e4653d7
Dominick <domenico.damato@vtsd.com>
2022-11-01 10:37:19 -0400

    Updated files as of 11/1/22.

--- Lab05vst.java
+++ Lab05vst.java
@@ -24,20 +24,21 @@ public class Lab05vst extends Applet
             x++;
 
             g.drawLine(x1, y1, x2, y2);
-            y2 -= 10;
-            x1 += 10;
+            y2 -= 12;
+            x1 += 19;
         }
 
         x1 = 990;
         y1 = 640;
         x2 = 10;
         y2 = 640;
+        x = 1;
         while (x <= 50) {
             x++;
 
             g.drawLine(x1, y1, x2, y2);
-            y2 -= 10;
-            x1 -= 10;
+            y2 -= 12;
+            x1 -= 19;
         }
 
 
